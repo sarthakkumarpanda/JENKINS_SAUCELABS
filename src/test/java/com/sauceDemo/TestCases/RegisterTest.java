5305771e1d1e201115523c5bf99b9658c8e61a84
sarthakkumarpanda <sarthak.matrix@gmail.com>
2024-05-12 10:24:20 -0400

    pushing Register test

---
+++ src/test/java/com/sauceDemo/TestCases/RegisterTest.java
@@ -0,0 +1,12 @@
+package com.sauceDemo.TestCases;
+
+import com.sauceDemo.TestBase.TestBase;
+
+public class RegisterTest extends TestBase{
+
+	public RegisterTest() throws Exception {
+		super();
+		
+	}
+
+}
